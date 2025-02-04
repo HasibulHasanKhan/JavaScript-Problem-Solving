53df6c316d5eaeab983495559c859dfd2448495a
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-04 16:44:28 +0600

    *

--- JavaScript/String/reverse.js
+++ JavaScript/String/reverse.js
@@ -10,3 +10,14 @@ function reverseString(str) {
 // Example usage:
 console.log(reverseString("hello")); // Output: "olleh"
 console.log(reverseString("JavaScript")); // Output: "tpircSavaJ"
+
+// Using a for Loop (Manual Reversal)
+
+function reverseStringLoop(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+console.log(reverseStringLoop("hello"));
