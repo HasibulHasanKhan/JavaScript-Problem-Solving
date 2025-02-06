4de8ede6330cee9135b5831460e6497776a0408d
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-07 01:21:47 +0600

    missingNumber

---
+++ JavaScript/Algorithm/missingNumber.js
@@ -0,0 +1,13 @@
+// problem : Write a function that finds the missing number in a given integer array containing numbers from 1 to n.
+
+function findMissingNumber(arr) {
+  let expectedNum = (n * (n + 1)) / 2;
+
+  let actualSum = arr.reduce((sum, num) => sum + num, 0);
+
+  return expectedNum - actualSum;
+}
+
+const arr = [1, 2, 4, 5, 6];
+const n = 6;
+console.log(findMissingNumber(arr, n));
