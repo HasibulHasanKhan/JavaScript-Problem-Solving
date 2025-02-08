ab6eea97035f4705985288ae8cef6a5453df0057
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-08 21:42:29 +0600

    *

--- JavaScript/HighOrderFunction/functionCaching.js
+++ JavaScript/HighOrderFunction/functionCaching.js
@@ -1,7 +1,8 @@
 function memoize(fn) {
   const cache = new Map();
-
+  // Returns a new function that wraps 'fn'
   return function (...args) {
+    //...args collects the arguments into an array: that why key inside [].
     const key = JSON.stringify(args);
 
     if (cache.has(key)) {
@@ -32,3 +33,19 @@ console.log(cachedFactorial(5)); // Fetches from cache (improved performance)
 console.log(cachedFactorial(6)); //
 // Calculates new value
 cachedFactorial(7);
+
+//---------------------------
+async function fetchData(url) {
+  const response = await fetch(url);
+  return response.json();
+}
+
+// Create a cached version of fetchData
+const cachedFetch = memoize(fetchData);
+
+cachedFetch("https://jsonplaceholder.typicode.com/todos/1").then((data) =>
+  console.log(data)
+);
+
+cachedFetch("https://jsonplaceholder.typicode.com/todos/2") // From cache
+  .then((data) => console.log(data));
