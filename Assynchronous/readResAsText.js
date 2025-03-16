aa1f53059d1179f455a1121ddcfcc946061f7de6
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-03-16 23:33:14 +0600

    .

--- Assynchronous/readResAsText.js
+++ Assynchronous/readResAsText.js
@@ -1,7 +1,27 @@
-async function fetchText(url) {
+// async function fetchText(url) {
+//   let response = await fetch(url);
+//   let textData = await response.text(); // Read response as text
+//   console.log("Text Response:", textData);
+// }
+
+// fetchText("https://jsonplaceholder.typicode.com/todos/1");
+
+// .blob() - read as binary data (image, pdfs,otc)
+// async function fetchImage(url) {
+//   let response = await fetch(url);
+//   let blobData = await response.blob(); // Read response as binary blob
+//   let imgURL = URL.createObjectURL(blobData); // Create URL for the blob
+//   console.log("Image Blob URL:", imgURL);
+// }
+
+// fetchImage("https://via.placeholder.com/150");
+
+//arrayBuffer() - Read as Raw Binary Data
+
+async function fetchBinary(url) {
   let response = await fetch(url);
-  let textData = await response.text(); // Read response as text
-  console.log("Text Response:", textData);
+  let bufferData = await response.arrayBuffer(); // Read as raw binary
+  console.log("ArrayBuffer Length:", bufferData.byteLength);
 }
 
-fetchText("https://jsonplaceholder.typicode.com/todos/1");
+fetchBinary("https://via.placeholder.com/150");
